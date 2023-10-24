604d1089bd261ce3a864953133fab9c5b3edc768
unknown <unexist-email>
2023-10-24 16:58:17 +0300

    note-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,10 @@
 import { createSignal } from "solid-js";
-import { pinyin } from "./convert.utils";
+import NotePage from "./NotePage/NotePage";
 import "./App.css";
 
-// 切的图标集 由
-
-const resSpace = (char) => (char === " " ? " " : char);
-
-const convert = (data) => {
-  const rows = data.split("\n");
-
-  return rows.map((row) => {
-    const hanziArr = row.split("").map(resSpace);
-
-    return hanziArr.map((item) => ({ hanzi: item, pinyin: pinyin(item) }));
-  });
-};
-
 function App() {
-  const [isPinyinVisible, setPinyinVisibility] = createSignal(true);
-  const [text, setText] = createSignal("由");
-
   return (
-    <>
-    <div class="editor">
-      <textarea
-        value={text()}
-        onInput={(e) => {
-          console.log(e.currentTarget.value);
-          setText(e.currentTarget.value);
-        }}
-      ></textarea>
-      <div>
-        <button onClick={() => setPinyinVisibility(!isPinyinVisible())}>
-          {isPinyinVisible() ? 'Hide Pinyin' : 'Show Pinyin'}
-        </button>
-      </div>
-    </div>
-
-      <div class="reader">
-        {convert(text()).map((row) => (
-          <div class="row">
-            {row.map((item) => (
-              <ruby>
-                <span class="hanzi">{item.hanzi}</span>
-                {isPinyinVisible() ? (
-                  <rt>
-                    <span class="pinyin">{item.pinyin}</span>
-                  </rt>
-                ) : null}
-              </ruby>
-            ))}
-          </div>
-        ))}
-      </div>
-    </>
+    <NotePage noteId="1"></NotePage>
   );
 }
 
