37a95ba67277e14f9d2411b64243502806982ffb
unknown <unexist-email>
2023-10-25 16:07:48 +0300

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,75 @@
 import { createSignal } from "solid-js";
 import NotePage from "./NotePage/NotePage";
+import Menu from "./Menu/Menu";
+
 import "./App.css";
+import {
+  addNoteToStorage,
+  getNotesFromStorage,
+  removeNote,
+  updateNote,
+} from "./storage.utils";
 
-const customerData = [
-  {
-    text: "切的图标集 由",
-  },
-  {
-    text: "切的图\n标集 由切的\n图标集 由切的图\n标集 由",
-  },
-];
-
-localStorage.setItem("pinyin-data", JSON.stringify(customerData));
+// const customerData = [
+//   {
+//     text: "切的图标集 由",
+//   },
+//   {
+//     text: "切的图\n标集 由切的\n图标集 由切的图\n标集 由",
+//   },
+// ];
 
 function App() {
   const [menuToggle, setMenuOpen] = createSignal(false);
-  const [noteText, setNoteText] = createSignal("");
+  const [notes, setNotes] = createSignal(getNotesFromStorage());
+  const [currNoteIndex, setCurrNoteIndex] = createSignal(-1);
+  const currNoteText = () => {
+    return notes().find((_, index) => currNoteIndex() === index);
+  };
 
-  const rawData = localStorage.getItem("pinyin-data");
-  const parsedData = rawData ? JSON.parse(rawData) : [];
+  const createNewNote = (text) => {
+    setCurrNoteIndex(0);
+    addNoteToStorage(text);
+    setNotes(getNotesFromStorage());
+  };
+  const setNoteIndex = (index) => {
+    setCurrNoteIndex(index);
+  };
+  const removeNote1 = (index) => {
+    removeNote(index);
+
+    setNotes(getNotesFromStorage());
+  };
+  const updateNote1 = (index, text) => {
+    updateNote(index, text);
+
+    setNotes(getNotesFromStorage());
+  };
 
   return (
     <>
       <div class="menu-button" onClick={() => setMenuOpen(!menuToggle())}>
-        M
+        {"⯇"}
       </div>
       {menuToggle() ? (
-        <div class="menu">
-          <div>MENU</div>
-          {parsedData.map((item) => (
-            <div
-              class="menu-item"
-              onClick={() => {
-                setNoteText(item.text);
-                setMenuOpen(false);
-              }}
-            >
-              {item.text}
-            </div>
-          ))}
-        </div>
+        <Menu
+          notes={notes}
+          setNoteIndex={setNoteIndex}
+          createNewNote={createNewNote}
+          removeNote={removeNote1}
+          setMenuOpen={setMenuOpen}
+        ></Menu>
       ) : null}
-      <NotePage noteText={noteText} setNoteText={setNoteText}></NotePage>
+      {currNoteIndex() >= 0 ? (
+        <NotePage
+          noteText={currNoteText}
+          setNoteText={(text) => updateNote1(currNoteIndex(), text)}
+        ></NotePage>
+      ) : (
+        <button class="new-n-btn" onClick={() => createNewNote("New note")}>
+          Create new note
+        </button>
+      )}
     </>
   );
 }
